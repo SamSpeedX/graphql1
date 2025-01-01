b388172865e473cd7ceb1809b2a314e1b6f75dcc
SAM OCHU <109700174+SamSpeedX@users.noreply.github.com>
2025-01-01 14:24:35 +0000

    new

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'sam/graphql',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '32bd41547c47e6e65b3ccaf2b40a7d95fe45d1cf',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'sam/graphql' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '32bd41547c47e6e65b3ccaf2b40a7d95fe45d1cf',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'webonyx/graphql-php' => array(
+            'pretty_version' => 'v15.19.1',
+            'version' => '15.19.1.0',
+            'reference' => 'fa01712b1a170ddc1d92047011b2f4c2bdfa8234',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../webonyx/graphql-php',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
